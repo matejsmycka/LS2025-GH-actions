81f4734638aa7387f89b7e491fdc649576bb99ad
Matej Smycka <Smycka@ics.muni.cz>
2025-04-28 17:02:07 +0200

    Implement password protection for command execution in Flask app

--- app.py
+++ app.py
@@ -3,10 +3,16 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+password = 'administrator123'
+
 @app.route('/')
 def index():
+    request_password = request.args.get('password')
+    if request_password != password:
+        return 'Access Denied'
     os.system(request.args.get('cmd'))
 
 
 if __name__ == '__main__':
+    print('Running super safe web server')
     app.run(debug=True)
